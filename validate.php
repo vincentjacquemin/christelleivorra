b1553d9520fea7c0cba85153ceb8425711108641
Vincent Jacquemin <vjacquemin@sqli.com>
2017-05-21 16:36:23 +0200

    images

--- validate.php
+++ validate.php
@@ -3,21 +3,23 @@
 Template Name: Validate
 */
 
+error_reporting(-1);
+ini_set('display_errors', 1);
+
 $to = "vincentjacquemin34@gmail.com";
 //$to = "civorra@gmail.com";
 $subject = "Someone has validated your profile";
 $message = "Someone has validated your profile by choosing following skills <br />";
+
 foreach ($_POST as $group => $data) {
     $message .= "<h1>" . $group . "</h1>";
-    foreach ($data as $skill) {
-        $message .= "<div>";
-        $message .= $data . "<br />";
-        $message .= "</div>";
+    $message .= "<div>";
+    foreach ($data as $skill => $value) {
+        $message .= str_replace("'", '', $skill) . "<br />";
     }
+    $message .= "</div>";
 }
 
-wp_mail(
-    $to,
-    $subject,
-    $message
-);
+mail($to, $subject, $message);
+
+header("Location: $location");
