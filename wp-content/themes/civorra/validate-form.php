60f180ca216d214ea31b0c323a8fbb23a5a2c7e9
Vincent Jacquemin <vjacquemin@sqli.com>
2017-07-07 07:27:35 +0200

    Styl

--- wp-content/themes/civorra/validate-form.php
+++ wp-content/themes/civorra/validate-form.php
@@ -19,7 +19,7 @@ $skillsMatching = [
     'collaboration' => 'Collaboration efficace avec des partenaires aux profils variés'
 ];
 
-$website = 'http://test.civorra.fr';
+$website = 'http://www.christelleivorra.com';
 
 if (!isset($_POST) || empty($_POST)) {
     header('Location: http://www.civorra.fr');
@@ -63,7 +63,7 @@ else {
         <link rel="alternate" type="application/rss+xml" title="Christelle Ivorra &raquo; Flux" href="<?php echo $website ?>/feed/" />
         <link rel="alternate" type="application/rss+xml" title="Christelle Ivorra &raquo; Flux des commentaires" href="<?php echo $website ?>/comments/feed/" />
         <script type="text/javascript">
-            window._wpemojiSettings = {"baseUrl":"https:\/\/s.w.org\/images\/core\/emoji\/2.3\/72x72\/","ext":".png","svgUrl":"https:\/\/s.w.org\/images\/core\/emoji\/2.3\/svg\/","svgExt":".svg","source":{"concatemoji":"http:\/\/test.civorra.fr\/wp-includes\/js\/wp-emoji-release.min.js?ver=4.8"}};
+            window._wpemojiSettings = {"baseUrl":"https:\/\/s.w.org\/images\/core\/emoji\/2.3\/72x72\/","ext":".png","svgUrl":"https:\/\/s.w.org\/images\/core\/emoji\/2.3\/svg\/","svgExt":".svg","source":{"concatemoji":"http:\/\/www.christelleivorra.com\/wp-includes\/js\/wp-emoji-release.min.js?ver=4.8"}};
             !function(a,b,c){function d(a){var b,c,d,e,f=String.fromCharCode;if(!k||!k.fillText)return!1;switch(k.clearRect(0,0,j.width,j.height),k.textBaseline="top",k.font="600 32px Arial",a){case"flag":return k.fillText(f(55356,56826,55356,56819),0,0),b=j.toDataURL(),k.clearRect(0,0,j.width,j.height),k.fillText(f(55356,56826,8203,55356,56819),0,0),c=j.toDataURL(),b===c&&(k.clearRect(0,0,j.width,j.height),k.fillText(f(55356,57332,56128,56423,56128,56418,56128,56421,56128,56430,56128,56423,56128,56447),0,0),b=j.toDataURL(),k.clearRect(0,0,j.width,j.height),k.fillText(f(55356,57332,8203,56128,56423,8203,56128,56418,8203,56128,56421,8203,56128,56430,8203,56128,56423,8203,56128,56447),0,0),c=j.toDataURL(),b!==c);case"emoji4":return k.fillText(f(55358,56794,8205,9794,65039),0,0),d=j.toDataURL(),k.clearRect(0,0,j.width,j.height),k.fillText(f(55358,56794,8203,9794,65039),0,0),e=j.toDataURL(),d!==e}return!1}function e(a){var c=b.createElement("script");c.src=a,c.defer=c.type="text/javascript",b.getElementsByTagName("head")[0].appendChild(c)}var f,g,h,i,j=b.createElement("canvas"),k=j.getContext&&j.getContext("2d");for(i=Array("flag","emoji4"),c.supports={everything:!0,everythingExceptFlag:!0},h=0;h<i.length;h++)c.supports[i[h]]=d(i[h]),c.supports.everything=c.supports.everything&&c.supports[i[h]],"flag"!==i[h]&&(c.supports.everythingExceptFlag=c.supports.everythingExceptFlag&&c.supports[i[h]]);c.supports.everythingExceptFlag=c.supports.everythingExceptFlag&&!c.supports.flag,c.DOMReady=!1,c.readyCallback=function(){c.DOMReady=!0},c.supports.everything||(g=function(){c.readyCallback()},b.addEventListener?(b.addEventListener("DOMContentLoaded",g,!1),a.addEventListener("load",g,!1)):(a.attachEvent("onload",g),b.attachEvent("onreadystatechange",function(){"complete"===b.readyState&&c.readyCallback()})),f=c.source||{},f.concatemoji?e(f.concatemoji):f.wpemoji&&f.twemoji&&(e(f.twemoji),e(f.wpemoji)))}(window,document,window._wpemojiSettings);
         </script>
         <style type="text/css">
@@ -109,8 +109,8 @@ else {
         <meta name="generator" content="WordPress 4.8" />
         <link rel="canonical" href="<?php echo $website ?>/" />
         <link rel='shortlink' href='<?php echo $website ?>/' />
-        <link rel="alternate" type="application/json+oembed" href="<?php echo $website ?>/wp-json/oembed/1.0/embed?url=http%3A%2F%2Ftest.civorra.fr%2F" />
-        <link rel="alternate" type="text/xml+oembed" href="<?php echo $website ?>/wp-json/oembed/1.0/embed?url=http%3A%2F%2Ftest.civorra.fr%2F&#038;format=xml" />
+        <link rel="alternate" type="application/json+oembed" href="<?php echo $website ?>/wp-json/oembed/1.0/embed?url=http%3A%2F%2Fwww.christelleivorra.com%2F" />
+        <link rel="alternate" type="text/xml+oembed" href="<?php echo $website ?>/wp-json/oembed/1.0/embed?url=http%3A%2F%2Fwww.christelleivorra.com%2F&#038;format=xml" />
         <style type="text/css">.recentcomments a{display:inline !important;padding:0 !important;margin:0 !important;}</style>
         <style type="text/css" media="print">#wpadminbar { display:none; }</style>
     </head>
